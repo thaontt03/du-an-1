e4d696f2045bb661ba73544f79a1ce97a264d84e
anhtitan <phamducanhofficial@gmail.com>
2022-11-17 17:27:58 +0700

    update join bảng người dùng với chức vụ

--- src/main/java/edu/poly/duan1/model/ChucVu.java
+++ src/main/java/edu/poly/duan1/model/ChucVu.java
@@ -5,12 +5,17 @@
 package edu.poly.duan1.model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -42,4 +47,7 @@ public class ChucVu {
 
     @Column
     private Date ngaySua;
+
+    @OneToMany(mappedBy = "chucVu", fetch = FetchType.LAZY)
+    List<NguoiDung> listND;
 }
--- src/main/java/edu/poly/duan1/model/NguoiDung.java
+++ src/main/java/edu/poly/duan1/model/NguoiDung.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -44,6 +46,10 @@ public class NguoiDung {
 
     @Column
     private String matKhau;
+
+    @ManyToOne
+    @JoinColumn(name = "idCV")
+    private ChucVu chucVu;
 
     @Column
     private Date ngayTao;
