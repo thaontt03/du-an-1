73240af45383c58e55e8601498d313f82bf9dbaf
anhtitan <phamducanhofficial@gmail.com>
2022-11-23 21:49:33 +0700

    update Applicaiton

--- src/main/java/edu/poly/Application/Application.java
+++ src/main/java/edu/poly/Application/Application.java
@@ -13,6 +13,16 @@ import edu.poly.duan1.view.view_Login;
 public class Application {
 
     public static void main(String[] args) {
-        new view_Login().setVisible(true);
+       try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+       new view_Login().setVisible(true);
     }
 }
